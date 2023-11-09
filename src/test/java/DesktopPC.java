7cf2429e9f52c4068600531222633c77729233b6
Burak Yılmam <61635780+Burakyilmam@users.noreply.github.com>
2023-11-09 17:42:17 +0300

    Elektronik içerisinde bulunan bilgisayar/tablet kategori içerisinde bulunan masaüstü bilgisayar kategorisi açıldı.

---
+++ src/test/java/DesktopPC.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class DesktopPC {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.hepsiburada.com");
+        driver.manage().window().maximize();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        Actions actions = new Actions(driver);
+
+        WebElement elektronik = driver.findElement(By.xpath("//span[contains(text(), 'Elektronik')]"));
+        actions.moveToElement(elektronik).perform();
+
+        WebElement bilgisayar = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[text()='Bilgisayar/Tablet']")));
+        actions.moveToElement(bilgisayar).perform();
+
+        WebElement DesktopPC = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[text()='Masaüstü Bilgisayar']")));
+        actions.moveToElement(DesktopPC).perform();
+        DesktopPC.click();
+    }
+}
